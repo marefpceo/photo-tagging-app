4a585ebc7bc0e98791b5400fba32df9191fd9c94
marefpceo <marefpceo@gmail.com>
2024-11-01 15:49:06 -0700

    Updated seed file
    Updated seed file with first game_image to test file's functionality.
    Seed works correctly and upload to database was succesful. Ready for
    the remaining entries.

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -2,8 +2,20 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-
-}
+  const beach_waldo = await prisma.game_image.upsert({
+    where: {
+      id: 1,
+    },
+    update: {},
+    create: {
+      image_location: 'images/beach_waldo.jpeg',
+      title: 'Beach',
+      width: 3000,
+      height: 1926
+    }
+  });
+  console.log({beach_waldo});
+};
 
 main()
   .then(async () => {
