5d940ca7d5485b359c5b294ad3695d58981bd5a1
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-10-31 17:47:28 -0700

    Removed unused tiles and updated eslint config

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -18,7 +17,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // Catch 404 errors and forward to the error handler
 app.use(function (req, res, next) {
