fd97f512045a8a938dbdd64a564a132e6ee5ce1b
marefpceo <marefpceo@gmail.com>
2024-11-01 19:32:17 -0700

    Created query to return game image list

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -1,7 +1,16 @@
 const asyncHandler = require('express-async-handler');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
+// Return all image game images
 exports.image_list_get = asyncHandler(async (req, res, next) => {
+  const image_list = await prisma.game_image.findMany({
+    include: {
+      characters: true,
+      game_leaders: true,
+    }
+  });
   res.json({
-    image_list: 'Image List',
+    image_list
   });
 });
