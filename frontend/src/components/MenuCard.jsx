28ee37588f3233ad3f7c1df035e7ab6e09f6413d
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-04-06 17:30:51 -0700

    Moved Leader Board button from Header to MenuCard

--- frontend/src/components/MenuCard.jsx
+++ frontend/src/components/MenuCard.jsx
@@ -1,20 +1,30 @@
 import Button from './Button';
 import { useNavigate } from 'react-router-dom';
 
-function MenuCard({ image_location, id, title, width, height, characters, startGame }) {
+function MenuCard({
+  image_location,
+  id,
+  title,
+  width,
+  height,
+  characters,
+  startGame,
+}) {
   const navigate = useNavigate();
 
   function handleClick() {
     startGame(id);
-    navigate('/game', { state: {
-      imageId: id,
-      image_location: image_location,
-      title: title,
-      width: width,
-      height: height,
-      characters: characters,
-    }});
-  };
+    navigate('/game', {
+      state: {
+        imageId: id,
+        image_location: image_location,
+        title: title,
+        width: width,
+        height: height,
+        characters: characters,
+      },
+    });
+  }
 
   return (
     <div
@@ -29,15 +39,25 @@ function MenuCard({ image_location, id, title, width, height, characters, startG
           className='h-44 w-full'
         />
       </div>
-      <Button
-        type={'button'}
-        id={'menuCardBtn'}
-        text={'Select'}
-        className={
-          'mt-4 h-10 w-28 rounded-md bg-red-500 font-bold text-white hover:shadow-lg'
-        }
-        onClick={handleClick}
-      />
+      <div className='mt-4 flex items-center justify-evenly'>
+        <Button
+          type={'button'}
+          id={'menuCardBtn'}
+          text={'Select'}
+          className={
+            'h-10 w-20 rounded-md bg-red-500 font-bold text-white hover:shadow-lg'
+          }
+          onClick={handleClick}
+        />
+        <Button
+          type={'button'}
+          id={'leaderBoardBtn'}
+          text={'Leader Board'}
+          className={
+            'h-10 w-28 rounded-md bg-sky-500 p-1 font-bold text-white shadow-md shadow-slate-400'
+          }
+        />
+      </div>
     </div>
   );
 }
--- frontend/src/sections/Header.jsx
+++ frontend/src/sections/Header.jsx
@@ -1,17 +1,7 @@
-import Button from '../components/Button';
-
 function Header() {
   return (
     <header className='flex justify-between'>
       <h1>Where&apos;s Waldo: Photo Tagging App</h1>
-      <Button
-        type={'button'}
-        id={'leaderBoardBtn'}
-        text={'Leader Board'}
-        className={
-          'h-10 w-32 rounded-md bg-sky-500 p-1 font-bold text-white shadow-md shadow-slate-400'
-        }
-      />
     </header>
   );
 }
