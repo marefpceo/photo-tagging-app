b110b982ea83fb7def1785ad1604c930e1671be4
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-04-28 19:47:14 -0700

    Updated cors options

--- server/app.js
+++ server/app.js
@@ -40,7 +40,7 @@ app.use(compression());
 //   next();
 // });
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
