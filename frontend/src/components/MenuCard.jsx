735edcd59db895ba4a483b846305c48db51bd46d
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-03-31 21:13:41 -0700

    Updating styling

--- frontend/src/components/MenuCard.jsx
+++ frontend/src/components/MenuCard.jsx
@@ -1,24 +1,34 @@
 import Button from './Button';
 import { useNavigate } from 'react-router-dom';
 
-function MenuCard({ image_location, id, title, width, height, characters, startGame }) {
+function MenuCard({
+  image_location,
+  id,
+  title,
+  width,
+  height,
+  characters,
+  startGame,
+}) {
   const navigate = useNavigate();
 
   function handleClick() {
     startGame(id);
-    navigate('/game', { state: {
-      imageId: id,
-      image_location: image_location,
-      title: title,
-      width: width,
-      height: height,
-      characters: characters,
-    }});
-  };
+    navigate('/game', {
+      state: {
+        imageId: id,
+        image_location: image_location,
+        title: title,
+        width: width,
+        height: height,
+        characters: characters,
+      },
+    });
+  }
 
   return (
     <div
-      className='max-w-1/5 m-0 rounded-sm border border-slate-100 bg-slate-100 pb-4 shadow-md shadow-slate-500'
+      className='m-0 max-w-1/5 rounded-sm border border-slate-100 bg-slate-100 pb-4 shadow-md shadow-slate-500'
       id={id}
     >
       <h3 className='p-2'>{title}</h3>
@@ -34,7 +44,7 @@ function MenuCard({ image_location, id, title, width, height, characters, startG
         id={'menuCardBtn'}
         text={'Select'}
         className={
-          'mt-4 h-10 w-28 rounded-md bg-red-500 font-bold text-white hover:shadow-lg'
+          'mt-4 h-10 w-28 cursor-crosshair rounded-md bg-red-500 font-bold text-white hover:shadow-lg'
         }
         onClick={handleClick}
       />
